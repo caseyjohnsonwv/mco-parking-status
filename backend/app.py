7606deb090efa329482dd4b5da6db5d546481fcf
Casey Johnson <caseyjohnsonwv@gmail.com>
2024-05-19 20:51:55 -0400

    move api to new prefix

--- backend/app.py
+++ backend/app.py
@@ -37,7 +37,7 @@ async def lifespan(app: FastAPI):
     pass
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(root_path='/api', lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:8080',
+    API_BASE_URL: process.env.API_BASE_URL ?? 'http://localhost:8080/api',
 }
 
 export default CONFIG;
