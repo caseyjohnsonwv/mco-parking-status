b8ce1f566a7677ed076eb462f5492d9937c43fcb
Casey Johnson <caseyjohnsonwv@gmail.com>
2024-05-20 19:14:37 -0400

    attempt to display an image

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
     import "../app.css";
     import '@fortawesome/fontawesome-free/css/all.min.css';
+    import { page } from "$app/stores";
+
+    const faviconPath = `${$page.url.host}/favicon.png`
 </script>
+
+<svelte:head>
+    <meta property="og:image" content={faviconPath} />
+</svelte:head>
 
 <slot />
