f35a4e63c8a17b7ede9a1d000ddcb335fa2575c3
Casey <caseyjohnsonwv@gmail.com>
2024-05-25 17:53:23 -0400

    make refresh look nicer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,58 +22,60 @@
 </script>
 
 <div class="bg-zinc-900 text-zinc-200">
-    <div class="mx-auto max-w-xl w-screen min-h-screen px-4 py-10 justify-center flex flex-col space-y-7">
-        {#if locations}
-            <div class="text-center space-y-3 font-light">
-                <p class="text-3xl">MCO Parking Status</p>
-                <p class="text-xs">Refreshed at <span class="px-1.5 mx-0.5 rounded-sm bg-blue-800 font-medium">{refreshTimestamp.toUpperCase()}</span> Orlando time.</p>
-            </div>
-            <div class="space-y-5">
-                <LocationList locations={economyLocations}>
-                    <span slot="title">Economy Lots</span>
-                    <span slot="payment">Cash, card, or E-Pass/SunPass</span>
-                </LocationList>
-                <LocationList locations={surfaceLocations}>
-                    <span slot="title">Surface Lots</span>
-                    <span slot="payment">E-Pass/SunPass ONLY</span>
-                </LocationList>
-                <LocationList locations={garageLocations}>
-                    <span slot="title">Garages</span>
-                    <span slot="payment">Cash, card, or E-Pass/Sunpass</span>
-                </LocationList>
-                <LocationList locations={valetLocations}>
-                    <span slot="title">Valets</span>
-                    <span slot="payment">Cash or card ONLY. Open 6:00am - 11:00pm</span>
-                </LocationList>
-            </div>
-            <div class="space-y-3">
-                <div class="font-semibold max-w-64 mx-auto bg-zinc-100 text-zinc-900 p-1 pt-1.5 rounded-sm">
-                    <a href="https://orlandoairports.net/parking-transportation/parking/" target="_blank" class="flex flex-row justify-center space-x-2 items-center text-sm">
-                        <span>View on MCO website</span>
-                        <i class="fa-solid fa-arrow-up-right-from-square text-xs"></i>
+    <div class="mx-auto max-w-xl w-screen min-h-screen px-4 py-10 justify-center space-y-2">
+        <p class="text-center space-y-3 font-light text-3xl">MCO Parking Status</p>
+        <div class="space-y-5 text-center">
+            {#if locations}
+                <p class="text-center text-xs">Refreshed at <span class="px-1.5 mx-0.5 rounded-sm bg-blue-800 font-medium">{refreshTimestamp.toUpperCase()}</span> Orlando time.</p>
+                <div class="space-y-5">
+                    <LocationList locations={economyLocations}>
+                        <span slot="title">Economy Lots</span>
+                        <span slot="payment">Cash, card, or E-Pass/SunPass</span>
+                    </LocationList>
+                    <LocationList locations={surfaceLocations}>
+                        <span slot="title">Surface Lots</span>
+                        <span slot="payment">E-Pass/SunPass ONLY</span>
+                    </LocationList>
+                    <LocationList locations={garageLocations}>
+                        <span slot="title">Garages</span>
+                        <span slot="payment">Cash, card, or E-Pass/Sunpass</span>
+                    </LocationList>
+                    <LocationList locations={valetLocations}>
+                        <span slot="title">Valets</span>
+                        <span slot="payment">Cash or card ONLY. Open 6:00am - 11:00pm</span>
+                    </LocationList>
+                </div>
+                <div class="space-y-3">
+                    <div class="font-semibold max-w-64 mx-auto bg-zinc-100 text-zinc-900 p-1 pt-1.5 rounded-sm">
+                        <a href="https://orlandoairports.net/parking-transportation/parking/" target="_blank" class="flex flex-row justify-center space-x-2 items-center text-sm">
+                            <span>View on MCO website</span>
+                            <i class="fa-solid fa-arrow-up-right-from-square text-xs"></i>
+                        </a>
+                    </div>
+                    <div class="font-light max-w-64 mx-auto bg-blue-800 text-zinc-50 p-1 pt-1.5 rounded-sm">
+                        <a href="https://linkedin.com/in/caseyjohnsonwv" target="_blank" class="flex flex-row justify-center space-x-2 items-baseline text-sm">
+                            <span>Created by Casey Johnson</span>
+                            <i class="fa-brands fa-linkedin text-lg"></i>
+                        </a>
+                    </div>
+                </div>
+                <hr>
+                <div>
+                    <a href="https://venmo.com/u/caseyjohnsonwv" target="_blank">
+                        <div class="bg-zinc-800 text-zinc-400 text-sm flex flex-col justify-center text-center p-2 rounded-md">
+                            <span class="font-semibold mb-2">This page costs $97/year to operate.</span>
+                            <span class="font-light">Domain name registration: $13/year.</span>
+                            <span class="font-light">Server: $84/year (billed monthly for $7).</span>
+                            <span class="font-medium mt-2 underline">Donations welcome via Venmo!</span>
+                        </div>
                     </a>
                 </div>
-                <div class="font-light max-w-64 mx-auto bg-blue-800 text-zinc-50 p-1 pt-1.5 rounded-sm">
-                    <a href="https://linkedin.com/in/caseyjohnsonwv" target="_blank" class="flex flex-row justify-center space-x-2 items-baseline text-sm">
-                        <span>Created by Casey Johnson</span>
-                        <i class="fa-brands fa-linkedin text-lg"></i>
-                    </a>
+            {:else}
+                <div class="space-y-8">
+                    <p class="text-xs text-zinc-200">Refreshing...</p>
+                    <i class="fa-solid fa-spinner animate-spin"></i>
                 </div>
-            </div>
-            <hr>
-            <a href="https://venmo.com/u/caseyjohnsonwv" target="_blank">
-                <div class="bg-zinc-800 text-zinc-400 text-sm flex flex-col justify-center text-center p-2 rounded-md">
-                    <span class="font-semibold mb-2">This page costs $97/year to operate.</span>
-                    <span class="font-light">Domain name registration: $13/year.</span>
-                    <span class="font-light">Server: $84/year (billed monthly for $7).</span>
-                    <span class="font-medium mt-2 underline">Donations welcome via Venmo!</span>
-                </div>
-            </a>
-        {:else}
-            <div class="text-center font-light flex flex-col space-y-3">
-                <p class="text-xs text-zinc-200">Refreshing...</p>
-                <i class="fa-solid fa-spinner animate-spin"></i>
-            </div>
-        {/if}
+            {/if}
+        </div>
     </div>
 </div>
