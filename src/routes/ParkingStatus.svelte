5ad9b564380009a83262da2ad8613450e5bb5f26
Casey <caseyjohnsonwv@gmail.com>
2024-05-25 17:57:03 -0400

    fix location titles

--- src/routes/ParkingStatus.svelte
+++ src/routes/ParkingStatus.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-row align-middle items-center justify-between text-xs font-semibold {isOpen ? 'bg-green-500' : 'bg-red-600'} py-1 px-2 rounded-sm">
-    <div class="w-2/3">
+    <div class="w-2/3 text-left">
         {props.name}
     </div>
     <div class="flex flex-row align-middle items-center space-x-5 w-1/3 justify-end">
